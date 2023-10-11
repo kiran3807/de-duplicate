1e19b0e6bfd700e2204008dd33fa9c2de20e2f5c
Kiran Yallabandi <kiranyallabandi@gmail.com>
2023-10-11 07:30:38 +0530

    Added the basic code to update the system_status table while the main thread is busy

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { Client } from "pg";
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
 import * as dotenv from "dotenv";
 import assert from "assert";
 
-type DeDuplicatedRow = {
-    title : string,
-    sources : {
-        [key: string] : number[]
-    }
-}
-
-type ValidRow = {
-    title : string,
-    ids : number[]
-}
+import { DeDuplicatedRow, ValidRow, Logger } from "./types";
 
 async function getDBConnection() {
 
@@ -162,4 +153,70 @@ async function main() {
     }
 }
 
-main();
+//main();
+
+async function foomain() {
+
+    const startTime = Date.now();
+    const timeout = 60000;
+
+    if(isMainThread) {
+
+        console.log("in main thread");
+
+        const childWorker = new Worker(__filename, {});
+        childWorker.on("message", (msg)=> {
+
+            if(msg.status === Logger.LOGGER_ONLINE ) {
+                
+                console.log("Logger is online now");
+            } else if(msg.status === Logger.LOGGER_ONLINE) {
+                
+                console.log("Logger is offline now");
+            }
+        });
+        childWorker.postMessage({ loggerStatus: Logger.ONLINE });
+        
+        while (true) {
+            if (Date.now() - startTime >= timeout) {
+                console.log('Timeout exceeded');
+                break;
+            }
+        }
+        childWorker.postMessage({ loggerStatus: Logger.OFFLINE });
+        console.log("main thread has ended");
+        
+    } else {
+
+        let intervalHandler:any;
+        const conn = await getDBConnection();
+
+        parentPort!.on('message', async (message)=> {
+
+            if(message.loggerStatus === Logger.ONLINE) {
+
+                const insertQuery = `INSERT INTO system_status (timestamptz, is_system_up) VALUES ( $1, TRUE)`;
+                await conn.connect();
+                
+                parentPort?.postMessage({ status : Logger.LOGGER_ONLINE});
+
+                intervalHandler = setInterval( ()=> {
+
+                    conn.query(insertQuery, [new Date()]);
+
+                }, 2000);
+
+            }else if(message.loggerStatus === Logger.OFFLINE) {
+                
+                if(intervalHandler) {
+                    
+                    await conn.end();
+                    clearInterval(intervalHandler);
+                    parentPort?.postMessage({ status : Logger.LOGGER_OFFLINE});
+                }
+            }
+        });
+    }
+}
+
+foomain();
